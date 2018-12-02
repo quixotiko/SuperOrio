6c7b7d94ea46090d97571f17379f206b8db4571e
quixotiko <1686374648@qq.com>
2018-12-02 17:37:09 +0800

    Update all.js

--- all.js
+++ all.js
@@ -1,6 +1,8 @@
-var Sprite = function(type,behaviors){
+var Sprite = function(type,behaviors,src){
 	var default_width = 32;
 	    default_height = 32;
+	this.image = new Image();
+	this.image.src = src;
 	this.type = type;
 	this.behaviors = behaviors || [];
 	this.width = default_width;
@@ -9,10 +11,14 @@ var Sprite = function(type,behaviors){
 	this.left = 0;
 	this.velocityX = 0;
 	this.velocityY = 0;
+	this.offsetX = 0;
+	this.offsetY = 0;
 };
 
 Sprite.prototype = {
-
+	draw: function(context) {
+		context.drawImage(this.image,this.left,this.top);
+	}
 };
 
 var SuperOrio = function(){
@@ -60,32 +66,42 @@ var SuperOrio = function(){
 		height: this.platform_height,
 		width: 96,
 	}
-
+	,
+	{
+		
+		left: 100,
+		top: 200,
+		height: this.platform_height,
+		width: 96,
+	}
 	];
-
-
 };
 SuperOrio.prototype = {
+	initialize: function(){
+		this.initializeBackground();
+		// this.initializePlatforms();
+		this.startGame();
+	},
 	initializeBackground: function(){
 		this.backgroundImage.src = "images/background.png";
 		this.backgroundImage.onload = function(e){
 			superOrio.drawBackground();
 		};
 	},
-	initializePlatforms: function(){
-		for(var i = 0;i < this.platformData.length; i++){
-			(function(platform){
-				platform.platformImage = new Image();
-				platform.platformImage.src = "images/4rocks.png";
-				platform.platformImage.onload = function(e){
-				    superOrio.drawPlatform(platform);
-				};
-			})(this.platformData[i]);
-		}
-	},
-	drawPlatform: function(platform){
-		    this.context.drawImage(platform.platformImage,platform.left,platform.top);
-	},
+	// initializePlatforms: function(){
+	// 	for(var i = 0;i < this.platformData.length; i++){
+	// 		(function(platform){
+	// 			platform.platformImage = new Image();
+	// 			platform.platformImage.src = "images/4rocks.png";
+	// 			platform.platformImage.onload = function(e){
+	// 			    // superOrio.drawPlatform(platform);
+	// 			};
+	// 		})(this.platformData[i]);
+	// 	}
+	// },
+	// drawPlatform: function(platform){
+	// 	    this.context.drawImage(platform.platformImage,platform.left,platform.top);
+	// },
 	drawBackground: function(){
 		this.context.translate(0,this.bg_offset);
 		this.context.drawImage(this.backgroundImage,0,-1400);
@@ -100,42 +116,61 @@ SuperOrio.prototype = {
 	setOffset: function(now){
 		this.setBackgroundOffset(now);
 	},
+	setBackgroundOffset: function(now){
+		this.bg_offset = this.bg_v*(now-this.lastAnimationFrameTime)/1000;
+	},
 	draw: function(){
 		this.drawBackground();
-		for(var i = 0; i < this.platformData.length; i++){
-			this.drawPlatform(this.platformData[i]);
-		}
+		// for(var i = 0; i < this.platformData.length; i++){
+		// 	this.drawPlatform(this.platformData[i]);
+		// }
+		this.drawSprites();
 	},
 	startGame: function(){
 		requestNextAnimationFrame(superOrio.animate);
 	},
-	initialize: function(){
-		this.initializeBackground();
-		this.initializePlatforms();
-		this.startGame();
-	},
-	setBackgroundOffset: function(now){
-		this.bg_offset = this.bg_v*(now-this.lastAnimationFrameTime)/1000;
-	},
+	
 
 	createSprites: function(){
 		this.createOrioSprite();
-		this.initializeSprites();
+		this.createPlatformSprites();
+		// this.initializeSprites();
 	},
 	createOrioSprite: function(){
 		this.orio = new Sprite("orio",[
 			this.orioWalkBehavior, 
 			this.orioJumpBehavior, 
 			this.orioFallBehavior, 
-			this.orioCollideBehavior]);
+			this.orioCollideBehavior],"images/orio-right-standing.gif");
 		this.orio.width = 24;
 		this.orio.height = 32;
-		this.orio.left = 0;
-		this.orio.top = 600;
+		this.orio.left = 270;
+		this.orio.top = 544;
 
 		this.spritesArray.push(this.orio);
+	},
+	drawSprites: function() {
+		var sprites = this.spritesArray;
+		for(var i = 0; i < sprites.length; i++){
+			var sprite = sprites[i];
+			this.context.translate(0,sprite.offsetY);
+			sprite.draw(this.context);
+			this.context.translate(0,-sprite.offsetY);
+		}
+	},
+	createPlatformSprites: function(){
+		var platforms = this.platformData;
+		for(var i = 0; i < platforms.length; i++){
+			var sprite = new Sprite("platform", [], "images/4rocks.png");
+		    sprite.width = platforms[i].width;
+		    sprite.height = platforms[i].height;
+		    sprite.left = platforms[i].left;
+		    sprite.top = platforms[i].top;
+		    this.spritesArray.push(sprite);
+		}
 	}
 };
 
 var superOrio = new SuperOrio();
 superOrio.initialize();
+superOrio.createSprites();
