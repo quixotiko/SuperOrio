312a63df83eb04c6f883c08a75c76cae73a2bd70
quixotiko <1686374648@qq.com>
2018-12-03 20:15:26 +0800

    orio can jump now

--- all.js
+++ all.js
@@ -20,6 +20,10 @@ var Sprite = function(type,behaviors,src,cells,cellWidth,cellHeight){
 	this.velocityY = 0;
 	this.offsetX = 0;
 	this.offsetY = 0;
+	this.minLeftDistance = 0;
+	this.maxLeftDistance = 0;
+	this.minTopDistance =0;
+	this.maxTopDistance = 0;
 };
 
 Sprite.prototype = {
@@ -27,7 +31,8 @@ Sprite.prototype = {
 		if(this.cells < 2){
 			context.drawImage(this.image,this.left,this.top);
 		}
-		else{
+		else
+		{
 		if(this.lastCellMoveTime === 0){
 			this.lastCellMoveTime = now;
 		}
@@ -66,7 +71,7 @@ var SuperOrio = function(){
 	this.backgroundImage = new Image();
 
 	this.lastAnimationFrameTime = 0;
-	this.bg_v = 0;
+	this.bg_v = 10;
 	this.bg_offset = 0;
 
 
@@ -80,12 +85,14 @@ var SuperOrio = function(){
 
 	this.orio_walk_left = false;
 	this.orio_walk_right = false;
+	this.orio_jump = false;
 
 
 	this.platform_height = 32;
 	this.platformData = [
 	{
-		
+		type:"platform",
+		src:"images/3rocks.png",
 		left: 100,
 		top: 500,
 		height: this.platform_height,
@@ -94,13 +101,17 @@ var SuperOrio = function(){
 
 	{
 		
-		left: 0,
+		type:"platform",
+		src:"images/3rocks.png",
+		left: 100,
 		top: 400,
 		height: this.platform_height,
 		width: 96,
 	},
 	{
 		
+	    type:"platform",
+		src:"images/3rocks.png",
 		left: 100,
 		top: 300,
 		height: this.platform_height,
@@ -109,25 +120,30 @@ var SuperOrio = function(){
 	,
 	{
 		
-		left: 100,
+		type:"platform",
+		src:"images/platform01.png",
+		left: 0,
 		top: 200,
-		height: this.platform_height,
-		width: 96,
+		height: 48,
+		width: 16,
+		velocityX: 100,
+		minLeftDistance:0,
+		maxLeftDistance:300, 
 	}
 	];
 };
 SuperOrio.prototype = {
 	initialize: function(){
-		this.initializeBackground();
+		// this.initializeBackground();
 		// this.initializePlatforms();
 		this.startGame();
 	},
-	initializeBackground: function(){
-		this.backgroundImage.src = "images/background.png";
-		this.backgroundImage.onload = function(e){
-			superOrio.drawBackground();
-		};
-	},
+	// initializeBackground: function(){
+	// 	this.backgroundImage.src = "images/background.png";
+	// 	this.backgroundImage.onload = function(e){
+	// 		superOrio.drawBackground();
+	// 	};
+	// },
 	// initializePlatforms: function(){
 	// 	for(var i = 0;i < this.platformData.length; i++){
 	// 		(function(platform){
@@ -142,48 +158,116 @@ SuperOrio.prototype = {
 	// drawPlatform: function(platform){
 	// 	    this.context.drawImage(platform.platformImage,platform.left,platform.top);
 	// },
-	drawBackground: function(){
-		this.context.translate(0,this.bg_offset);
-		this.context.drawImage(this.backgroundImage,0,-1400);
-		this.context.translate(0,-this.bg_offset);
-	},
+	
 	animate: function(now){
 		superOrio.setOffset(now);
 		superOrio.draw(now);
-		lastAnimationFrameTime = now;
+		superOrio.lastAnimationFrameTime = now;
 		requestNextAnimationFrame(superOrio.animate);
 	},
+
 	setOffset: function(now){
-		this.setBackgroundOffset(now);
+		// this.setBackgroundOffset(now);
 		this.setSpritesOffset(now);
+		this.setOrioOffset(now);
 	},
-	setBackgroundOffset: function(now){
-		this.bg_offset = this.bg_v*(now-this.lastAnimationFrameTime)/1000;
-	},
+	// setBackgroundOffset: function(now){
+	// 	this.bg_offset = this.bg_v*(now-this.lastAnimationFrameTime)/1000;
+	// },
 	setSpritesOffset: function(now) {
 		for(var i = 0; i < this.spritesArray.length; i++){
 			var sprite = this.spritesArray[i];
+			if(sprite.left>sprite.maxLeftDistance || sprite.left<sprite.minLeftDistance){
+				sprite.velocityX = -sprite.velocityX;
+			}
 			sprite.offsetX = sprite.velocityX*(now-this.lastAnimationFrameTime)/1000;
 			sprite.offsetY = sprite.velocityY*(now-this.lastAnimationFrameTime)/1000;
 		}
 	},
+	setOrioOffset: function(now) {
+		for(var i = 0; i < this.spritesArray.length; i++){
+			if(this.spritesArray[i].type === "orio"){
+				var orio = this.spritesArray[i];
+				if(!orio.jumpping){
+					return;
+				}
+				if(orio.isGoingUp()){
+					if(!orio.isDoneGoingUp()){
+						var time = orio.upTimer.getElapsedTime();
+						var offsetY = (time /(orio.jumpDuration/2)) * orio.jumpHeight;
+						orio.top = orio.beforeJumpPosition - offsetY;
+					}
+					else{
+						orio.jumpClimax = orio.top;
+						orio.upTimer.stop();
+						orio.downTimer.start();
+					}
+				}
+				else if(orio.isGoingDown()){
+					if(!orio.isDoneGoingDown()){
+						var time = orio.downTimer.getElapsedTime();
+						var offsetY = (time /(orio.jumpDuration/2)) * orio.jumpHeight;
+							orio.top = orio.jumpClimax + offsetY;
+						
+					}
+					else{
+						orio.stopJump();
+						orio.top = orio.beforeJumpPosition;
+						if(orio.pre_left){orio.image.src = "images/orio-left-standing.gif"};
+						if(orio.pre_right){orio.image.src = "images/orio-right-standing.gif"}
+					}
+				}
+			}
+		}
+	},
+
 	draw: function(now){
-		this.drawBackground();
+		// this.drawBackground();
 		// for(var i = 0; i < this.platformData.length; i++){
 		// 	this.drawPlatform(this.platformData[i]);
 		// }
 		this.drawSprites(now);
 
 	},
+
+	// drawBackground: function(){
+	// 	this.context.translate(0,this.bg_offset);
+	// 	this.context.drawImage(this.backgroundImage,0,-1400);
+	// 	this.context.translate(0,-this.bg_offset);
+	// },
+	//根据每个sprite对象里面不同的offset绘画sprite对象，实现移动效果
+	drawSprites: function(now) {
+		var sprites = this.spritesArray;
+		for(var i = 0; i < sprites.length; i++){
+			var sprite = sprites[i];
+			sprite.left += sprite.offsetX;
+			sprite.top += sprite.offsetY;
+			sprite.draw(now,this.lastAnimationFrameTime,this.context);
+			
+		}
+	},
+
+
 	startGame: function(){
 		requestNextAnimationFrame(superOrio.animate);
 	},
 	
+
 // 创建sprite对象
 	createSprites: function(){
+		this.createBackgroundSprite();
 		this.createOrioSprite();
 		this.createPlatformSprites();
 		// this.initializeSprites();
+	},
+	createBackgroundSprite: function() {
+		this.background = new Sprite("background",[],"images/background.png",1,400,2000);
+		this.background.width = 400;
+		this.background.height = 2000;
+		this.background.left = 0;
+		this.background.top = -1400;
+
+		this.spritesArray.push(this.background);
 	},
 	createOrioSprite: function(){
 		this.orio = new Sprite("orio",[
@@ -195,76 +279,140 @@ SuperOrio.prototype = {
 		this.orio.height = 32;
 		this.orio.left = 270;
 		this.orio.top = 544;
-		this.orio_walk_left = false;
-		this.orio_walk_right = false;
+		this.orio.walk_left = false;
+		this.orio.walk_right = false;
+		this.orio.pre_left = false;
+		this.orio.pre_right = true;
+		
+		this.orio.jumpHeight = 150;
+		this.orio.jumpDuration = 1000;
+		this.orio.jumpping = false;
+		this.orio.upTimer = new Stopwatch();
+		this.orio.downTimer = new Stopwatch();
+		this.orio.jump = function() {
+			if(this.jumpping)
+				return;
+			this.jumpping = true;
+			this.beforeJumpPosition = this.top;
+			this.upTimer.start();
+		};
+		
+		this.orio.stopJump = function() {
+			this.jumpping = false;
+		};
+		this.orio.isGoingUp = function() {
+			return this.upTimer.isRunning();
+		};
+		this.orio.isDoneGoingUp = function(now) {
+			return this.upTimer.getElapsedTime() > this.jumpDuration/2;
+		};
+		this.orio.isGoingDown = function() {
+			return this.downTimer.isRunning();
+		};
+		this.orio.isDoneGoingDown = function(now) {
+			return this.downTimer.getElapsedTime() > this.jumpDuration/2;
+		};
 
 		this.spritesArray.push(this.orio);
 	},
 	createPlatformSprites: function(){
 		var platforms = this.platformData;
 		for(var i = 0; i < platforms.length; i++){
-			var sprite = new Sprite("platform", [], "images/3rocks.png");
+			var sprite = new Sprite(platforms[i].type, [], platforms[i].src,1,platforms[i].width,platforms[i].height);
 		    sprite.width = platforms[i].width;
 		    sprite.height = platforms[i].height;
 		    sprite.left = platforms[i].left;
 		    sprite.top = platforms[i].top;
+		    if(platforms[i].velocityX){sprite.velocityX = platforms[i].velocityX;}
+		    if(platforms[i].velocityY){sprite.velocityY = platforms[i].velocityY;}
+		    if(platforms[i].minLeftDistance){sprite.minLeftDistance = platforms[i].minLeftDistance}
+		    if(platforms[i].maxLeftDistance){sprite.maxLeftDistance = platforms[i].maxLeftDistance}
 		    this.spritesArray.push(sprite);
 		}
 	},
 
-
-	//根据每个sprite对象里面不同的offset绘画sprite对象，实现移动效果
-	drawSprites: function(now) {
-		var sprites = this.spritesArray;
-		for(var i = 0; i < sprites.length; i++){
-			var sprite = sprites[i];
-			this.context.translate(-sprite.offsetX,sprite.offsetY);
-			sprite.draw(now,this.lastAnimationFrameTime,this.context);
-			this.context.translate(sprite.offsetX,-sprite.offsetY);
-		}
-	}
 };
+
+
 window.onkeydown = function(e) {
 	var key = e.keyCode;
 	if(key === 65){
-		superOrio.orio_walk_left = true;
-		superOrio.orio.image.src = "images/orio-left-walking.png";
-		superOrio.orio.cells = 3;
-		superOrio.orio.cellWidth = 30;
-		superOrio.orio.cellHeight = 32;
+		superOrio.orio.walk_left = true;
+		
 	}else if(key === 68){
-		superOrio.orio_walk_right = true;
-		superOrio.orio.image.src = "images/orio-right-walking.png";
-		superOrio.orio.cells = 3;
-		superOrio.orio.cellWidth = 30;
-		superOrio.orio.cellHeight = 32;
+		superOrio.orio.walk_right = true;
+	}
+	else if(key === 87){
+		superOrio.orio.jump();
 	}
 };
 window.onkeyup = function(e) {
 	var key = e.keyCode;
 	if(key === 65){
-		superOrio.orio_walk_left = false;
+		superOrio.orio.walk_left = false;
+		superOrio.orio.pre_left = true;
+		superOrio.orio.pre_right = false;
 		superOrio.orio.image.src = "images/orio-left-standing.gif";
 		superOrio.orio.cells = 1;
 		superOrio.orio.cellWidth = 24;
 		superOrio.orio.cellHeight = 32;
 	}else if(key === 68){
-		superOrio.orio_walk_right = false;
+		superOrio.orio.walk_right = false;
+		superOrio.orio.pre_left =false;
+		superOrio.orio.pre_right = true;
 		superOrio.orio.image.src = "images/orio-right-standing.gif";
 		superOrio.orio.cells = 1;
 		superOrio.orio.cellWidth = 24;
 		superOrio.orio.cellHeight = 32;
 	}
+	else if(key === 87){
+		// superOrio.orio.jump = false;
+	}
 };
 //防止keydown时间停顿现象
 setInterval(function() {
-	if(superOrio.orio_walk_left){
+	if(superOrio.orio.walk_left&&!superOrio.orio.walk_right&&!superOrio.orio.jumpping){
 		superOrio.orio.left -= 4;
+		superOrio.orio.image.src = "images/orio-left-walking.png";
+		superOrio.orio.cells = 3;
+		superOrio.orio.cellWidth = 30;
+		superOrio.orio.cellHeight = 32;
 	}
-	else if(superOrio.orio_walk_right){
+	else if(superOrio.orio.walk_right&&!superOrio.orio.walk_left&&!superOrio.orio.jumpping){
 		superOrio.orio.left += 4;
+		superOrio.orio.image.src = "images/orio-right-walking.png";
+		superOrio.orio.cells = 3;
+		superOrio.orio.cellWidth = 30;
+		superOrio.orio.cellHeight = 32;
+	}
+	else if(superOrio.orio.jumpping&&superOrio.orio.walk_left&&!superOrio.orio.walk_right){
+		superOrio.orio.left -= 4;
+		superOrio.orio.image.src = "images/orio-left-jumping.gif";
+		superOrio.orio.cells = 1;
+		superOrio.orio.cellWidth = 32;
+		superOrio.orio.cellHeight = 32;
+	}else if(superOrio.orio.jumpping&&!superOrio.orio.walk_left&&superOrio.orio.walk_right){
+		superOrio.orio.left += 4;
+		superOrio.orio.image.src = "images/orio-right-jumping.gif";
+		superOrio.orio.cells = 1;
+		superOrio.orio.cellWidth = 32;
+		superOrio.orio.cellHeight = 32;
+	}
+	else if(superOrio.orio.jumpping&&!superOrio.orio.walk_left&&!superOrio.orio.walk_right){
+		if(superOrio.orio.pre_left){
+			superOrio.orio.image.src = "images/orio-left-jumping.gif";
+			superOrio.orio.cells = 1;
+		    superOrio.orio.cellWidth = 32;
+		    superOrio.orio.cellHeight = 32;
+		}else if(superOrio.orio.pre_right){
+			superOrio.orio.image.src = "images/orio-right-jumping.gif";
+			superOrio.orio.cells = 1;
+			superOrio.orio.cellWidth = 32;
+			superOrio.orio.cellHeight = 32;
+		}
 	}
 },30);
+
 var superOrio = new SuperOrio();
-superOrio.initialize();
 superOrio.createSprites();
+superOrio.initialize();
